a10d7355fa591fbc9e9a917f0f9d51da54f266e0
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-09-25 11:34:31 +0900

    SweetAlert2の導入 データベースからの削除処理、削除モーダル、削除後ポップアップの作成

--- src/main.js
+++ src/main.js
@@ -14,6 +14,18 @@ import 'popper.js';
 import 'bootstrap';
 import './assets/app.scss';
 
+// アラート
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+// アラート後のポップ
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+})
+window.Toast = Toast;
+
 
 Vue.component('Navbar', require('./components/Navbar.vue').default);
 // Vue.component('Hero', require('./components/Hero.vue').default);
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,132 +1,127 @@
 <template>
-  <div class="products">
-      <div class="container">
+<div class="products">
+  <div class="container">
 
-        <div class="intro h-100">
-            <div class="row h-100 justify-content-center align-items-center">
-              <div class="col-md-6">
-                    <h3>Products Page</h3>
+    <div class="intro h-100">
+      <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-md-6">
+          <h3>Products Page</h3>
 
-                 <p>
-                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, ducimus.
-                 </p>
-              </div>
-              <div class="col-md-6">
-                  <img src="/img/svg/products.svg" alt="" class="img-fluid">
-              </div>
-            </div>
-          </div>
-
-          <hr>
-
-          <div class="product-test">
-
-
-            <h3 class="d-inline-block">Products list</h3>
-            <button @click="addNew" class="btn btn-primary float-right">Add Product</button>
-
-            <div class="table-responsive">
-
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Price</th>
-                      <th>Modify</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                      <tr v-for="product in products">
-                        <td>
-                          {{product.name}}
-                        </td>
-
-                        <td>
-                          {{product.price}}
-                        </td>
-
-                        <td>
-
-                          <button class="btn btn-primary" @click="editProduct(product)">Edit</button>
-                          <button class="btn btn-danger" @click="deleteProduct(product)">Delete</button>
-                        </td>
-
-                      </tr>
-
-
-                  </tbody>
-                </table>
-            </div>
-
-          </div>
-      </div>
-
-      <!-- Modal -->
-      <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="editLabel">Edit Product</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-
-                <div class="row">
-                  <!-- main product -->
-                  <div class="col-md-8">
-                    <div class="form-group">
-                      <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <textarea name="description" class="form-control" placeholder="Product description" v-model='product.description' cols="30" rows="10"></textarea>
-                    </div>
-                  </div>
-                  <!-- product sidebar -->
-                  <div class="col-md-4">
-                    <h4 class="display-6">Product Details</h4>
-                    <hr>
-
-                    <div class="form-group">
-                      <input type="text" placeholder="Product price" v-model="product.price" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <input type="text" placeholder="Product tags" v-model="product.tag" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <label for="product_image">Product Images</label>
-                      <input type="file" @change="uploadImage()" class="form-control">
-                    </div>
-
-                  </div>
-                </div>
-
-
-
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button @click="addProduct()" type="button" class="btn btn-primary" v-if="modal == 'new'">Save changes</button>
-              <button @click="updateProduct()" type="button" class="btn btn-primary" v-if="modal == 'edit'">Apply changes</button>
-            </div>
-          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, ducimus.
+          </p>
+        </div>
+        <div class="col-md-6">
+          <img src="/img/svg/2.svg" alt="" class="img-fluid">
         </div>
       </div>
+    </div>
 
+    <hr>
 
+    <div class="product-test">
 
+      <h3 class="d-inline-block">Products list</h3>
+      <button @click="addNew" class="btn btn-primary float-right">Add Product</button>
+
+      <div class="table-responsive">
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Modify</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="product in products">
+              <td>
+                {{product.name}}
+              </td>
+
+              <td>
+                {{product.price}}
+              </td>
+
+              <td>
+
+                <button class="btn btn-primary" @click="editProduct(product)">Edit</button>
+                <button class="btn btn-danger" @click="deleteProduct(product)">Delete</button>
+              </td>
+
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+
+    </div>
   </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editLabel">Edit Product</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+
+          <div class="row">
+            <!-- main product -->
+            <div class="col-md-8">
+              <div class="form-group">
+                <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+              </div>
+
+              <div class="form-group">
+                <textarea name="description" class="form-control" placeholder="Product description" v-model='product.description' cols="30" rows="10"></textarea>
+              </div>
+            </div>
+            <!-- product sidebar -->
+            <div class="col-md-4">
+              <h4 class="display-6">Product Details</h4>
+              <hr>
+
+              <div class="form-group">
+                <input type="text" placeholder="Product price" v-model="product.price" class="form-control">
+              </div>
+
+              <div class="form-group">
+                <input type="text" placeholder="Product tags" v-model="product.tag" class="form-control">
+              </div>
+
+              <div class="form-group">
+                <label for="product_image">Product Images</label>
+                <input type="file" @change="uploadImage()" class="form-control">
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button @click="addProduct()" type="button" class="btn btn-info" v-if="modal == 'new'">Save changes</button>
+          <button @click="updateProduct()" type="button" class="btn btn-primary" v-if="modal == 'edit'">Apply changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script>
-
-import { fb, db} from '../firebase';
+import {
+  fb,
+  db
+} from '../firebase';
 
 export default {
   name: "Products",
@@ -134,60 +129,58 @@ export default {
     msg: String
   },
 
-  data(){
+  data() {
     return {
-        products: [],
-        product: {
-          name:null,
-          description:null,
-          price:null,
-          tag:null,
-          image:null
-        },
-        activeItem:null,
-        modal: null
+      // products: [],
+      product: {
+        name: null,
+        description: null,
+        price: null,
+        tag: null,
+        image: null
+      },
+      activeItem: null,
+      modal: null
     }
   },
 
-  firestore(){
-      return {
-        products: db.collection('products'),
-      }
+  firestore() {
+    return {
+      products: db.collection('products'),
+    }
   },
-  methods:{
+  methods: {
 
-    uploadImage(){},
+    uploadImage() {},
 
-    addNew(){
-        this.modal = 'new';
-        $('#product').modal('show');
+    addNew() {
+      this.modal = 'new';
+      $('#product').modal('show');
     },
-    updateProduct(){
-        this.$firestore.products.doc(this.product.id).update(this.product);
-          Toast.fire({
-            type: 'success',
-            title: 'Updated  successfully'
-          })
+    updateProduct() {
+      this.$firestore.products.doc(this.product.id).update(this.product);
+      Toast.fire({
+        type: 'success',
+        title: 'Updated  successfully'
+      })
 
-           $('#product').modal('hide');
+      $('#product').modal('hide');
     },
 
-    editProduct(product){
+    editProduct(product) {
       this.modal = 'edit';
       this.product = product;
       $('#product').modal('show');
     },
 
-
-    deleteProduct(doc){
-
+    deleteProduct(doc) {
 
       Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
         type: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#19a2b8',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!'
       }).then((result) => {
@@ -200,40 +193,34 @@ export default {
             title: 'Deleted  successfully'
           })
 
-
         }
       })
 
-
+    },
+    readData() {
 
     },
-    readData(){
-
-
-
-    },
-    addProduct(){
+    addProduct() {
 
       this.$firestore.products.add(this.product);
 
-          Toast.fire({
-            type: 'success',
-            title: 'Product created successfully'
-          })
+      Toast.fire({
+        type: 'success',
+        title: 'Product created successfully'
+      })
 
       $('#product').modal('hide');
     }
 
-
   },
-  created(){
-
+  created() {
 
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped lang="scss">
 
 </style>
